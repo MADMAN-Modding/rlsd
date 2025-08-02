da251f4cc5db7d9e3250fcefe13af7672fab53ab
MADMAN-Modding <mad@kmdtech.net>
2025-08-01 21:15:26 -0400

    added systemd and user creation to the setup script

--- install-scripts/linux/setup.sh
+++ install-scripts/linux/setup.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 # Written in [Amber](https://amber-lang.com/)
 # version: 0.4.0-alpha
-# date: 2025-08-01 20:35:12
+# date: 2025-08-01 21:15:11
+lowercase__10_v0() {
+    local text=$1
+    __AMBER_VAL_0=$( echo "${text}" | tr '[:upper:]' '[:lower:]' );
+    __AS=$?;
+    __AF_lowercase10_v0="${__AMBER_VAL_0}";
+    return 0
+}
 parse_number__12_v0() {
     local text=$1
      [ -n "${text}" ] && [ "${text}" -eq "${text}" ] 2>/dev/null ;
@@ -57,10 +64,29 @@ input_prompt__94_v0() {
     __AF_input_prompt94_v0="$REPLY";
     return 0
 }
-is_root__98_v0() {
-    __AMBER_VAL_0=$( id -u );
+input_confirm__96_v0() {
+    local prompt=$1
+    local default_yes=$2
+    local choice_default=$(if [ ${default_yes} != 0 ]; then echo " [\x1b[1mY/\x1b[0mn]"; else echo " [y/\x1b[1mN\x1b[0m]"; fi)
+             printf "\x1b[1m${prompt}\x1b[0m${choice_default}" ;
+        __AS=$?
+         read -s -n 1 ;
+        __AS=$?
+         printf "
+" ;
+        __AS=$?
+    __AMBER_VAL_1=$( echo $REPLY );
     __AS=$?;
-    if [ $([ "_${__AMBER_VAL_0}" != "_0" ]; echo $?) != 0 ]; then
+    lowercase__10_v0 "${__AMBER_VAL_1}";
+    __AF_lowercase10_v0__90_18="${__AF_lowercase10_v0}";
+    local result="${__AF_lowercase10_v0__90_18}"
+    __AF_input_confirm96_v0=$(echo $([ "_${result}" != "_y" ]; echo $?) '||' $(echo $([ "_${result}" != "_" ]; echo $?) '&&' ${default_yes} | bc -l | sed '/\./ s/\.\{0,1\}0\{1,\}$//') | bc -l | sed '/\./ s/\.\{0,1\}0\{1,\}$//');
+    return 0
+}
+is_root__98_v0() {
+    __AMBER_VAL_2=$( id -u );
+    __AS=$?;
+    if [ $([ "_${__AMBER_VAL_2}" != "_0" ]; echo $?) != 0 ]; then
         __AF_is_root98_v0=1;
         return 0
 fi
@@ -97,9 +123,9 @@ __1_RED="\e[31m"
 __2_GREEN="\e[32m"
 __3_BLUE="\e[34m"
 __4_RESET="\e[0m"
-__AMBER_ARRAY_1=("x86_64" "musl" "armv7" "aarch64");
-__5_architectures=("${__AMBER_ARRAY_1[@]}")
-echo_color__143_v0() {
+__AMBER_ARRAY_3=("x86_64" "musl" "armv7" "aarch64");
+__5_architectures=("${__AMBER_ARRAY_3[@]}")
+echo_color__144_v0() {
     local text=$1
     local color=$2
     # I'm not sure why but I need to do the reset on a separate concatenation
@@ -107,11 +133,11 @@ echo_color__143_v0() {
      echo -e "${__6_text}"${__4_RESET};
     __AS=$?
 }
-download_rlsd__144_v0() {
+download_rlsd__145_v0() {
     local version=$1
-    echo_color__143_v0 "Installing rlsd..." "${__3_BLUE}";
-    __AF_echo_color143_v0__23_5="$__AF_echo_color143_v0";
-    echo "$__AF_echo_color143_v0__23_5" > /dev/null 2>&1
+    echo_color__144_v0 "Installing rlsd..." "${__3_BLUE}";
+    __AF_echo_color144_v0__23_5="$__AF_echo_color144_v0";
+    echo "$__AF_echo_color144_v0__23_5" > /dev/null 2>&1
     file_exists__33_v0 "/usr/bin/rlsd";
     __AF_file_exists33_v0__25_8="$__AF_file_exists33_v0";
     if [ "$__AF_file_exists33_v0__25_8" != 0 ]; then
@@ -124,10 +150,49 @@ fi
     file_chmod__39_v0 "/usr/bin/rlsd" "+x";
     __AF_file_chmod39_v0__31_24="$__AF_file_chmod39_v0";
     local chmod_result="$__AF_file_chmod39_v0__31_24"
-    __AF_download_rlsd144_v0=$(echo ${download_result} '&&' ${chmod_result} | bc -l | sed '/\./ s/\.\{0,1\}0\{1,\}$//');
+    __AF_download_rlsd145_v0=$(echo ${download_result} '&&' ${chmod_result} | bc -l | sed '/\./ s/\.\{0,1\}0\{1,\}$//');
     return 0
 }
-setup__145_v0() {
+make_user__146_v0() {
+     useradd rlsd ;
+    __AS=$?
+    input_confirm__96_v0 "Setup RLSD config for clients? (if no, you'll have to su to the rlsd user later)" 1;
+    __AF_input_confirm96_v0__39_8="$__AF_input_confirm96_v0";
+    if [ "$__AF_input_confirm96_v0__39_8" != 0 ]; then
+        echo_color__144_v0 "Make sure your rlsd server is running and you know the IP" "${__3_BLUE}";
+        __AF_echo_color144_v0__40_9="$__AF_echo_color144_v0";
+        echo "$__AF_echo_color144_v0__40_9" > /dev/null 2>&1
+         rlsd --setup ;
+        __AS=$?
+        input_confirm__96_v0 "Setup systemd autostart service?" 1;
+        __AF_input_confirm96_v0__44_22="$__AF_input_confirm96_v0";
+        local choice="$__AF_input_confirm96_v0__44_22"
+        file_download__135_v0 "${__0_server_url}/install-scripts/linux/rlsd.service" "/etc/systemd/system/rlsd.service";
+        __AF_file_download135_v0__46_23="$__AF_file_download135_v0";
+        if [ $(echo ${choice} '&&' "$__AF_file_download135_v0__46_23" | bc -l | sed '/\./ s/\.\{0,1\}0\{1,\}$//') != 0 ]; then
+             systemctl daemon-reexec ;
+            __AS=$?
+             systemctl daemon-reload ;
+            __AS=$?
+             systemctl enable --now rlsd ;
+            __AS=$?;
+if [ $__AS != 0 ]; then
+                echo_color__144_v0 "Error enabling/starting RLSD service" "${__1_RED}";
+                __AF_echo_color144_v0__50_17="$__AF_echo_color144_v0";
+                echo "$__AF_echo_color144_v0__50_17" > /dev/null 2>&1
+fi
+else
+            echo_color__144_v0 "Error downloading systemd service" "${__1_RED}";
+            __AF_echo_color144_v0__53_13="$__AF_echo_color144_v0";
+            echo "$__AF_echo_color144_v0__53_13" > /dev/null 2>&1
+            __AF_make_user146_v0='';
+            return 2
+fi
+fi
+    __AF_make_user146_v0=1;
+    return 0
+}
+setup__147_v0() {
     local prompt="Please choose one of the following architectures:
 "
     i=0;
@@ -138,67 +203,80 @@ for arch in "${__5_architectures[@]}"; do
 done
     # This gets the user-input as a number
     input_prompt__94_v0 "${prompt}";
-    __AF_input_prompt94_v0__45_18="${__AF_input_prompt94_v0}";
-    local choice="${__AF_input_prompt94_v0__45_18}"
+    __AF_input_prompt94_v0__70_18="${__AF_input_prompt94_v0}";
+    local choice="${__AF_input_prompt94_v0__70_18}"
     parse_number__12_v0 "${choice}";
     __AS=$?;
 if [ $__AS != 0 ]; then
-__AF_setup145_v0=''
+__AF_setup147_v0=''
 return $__AS
 fi;
-    __AF_parse_number12_v0__47_18="$__AF_parse_number12_v0";
-    local choice="$__AF_parse_number12_v0__47_18"
+    __AF_parse_number12_v0__72_18="$__AF_parse_number12_v0";
+    local choice="$__AF_parse_number12_v0__72_18"
     if [ $(echo $(echo ${choice} '==' 0 | bc -l | sed '/\./ s/\.\{0,1\}0\{1,\}$//') '||' $(echo ${choice} '>' "${#__5_architectures[@]}" | bc -l | sed '/\./ s/\.\{0,1\}0\{1,\}$//') | bc -l | sed '/\./ s/\.\{0,1\}0\{1,\}$//') != 0 ]; then
-        echo_color__143_v0 "Invalid choice" "${__1_RED}";
-        __AF_echo_color143_v0__50_9="$__AF_echo_color143_v0";
-        echo "$__AF_echo_color143_v0__50_9" > /dev/null 2>&1
-        __AF_setup145_v0='';
+        echo_color__144_v0 "Invalid choice" "${__1_RED}";
+        __AF_echo_color144_v0__75_9="$__AF_echo_color144_v0";
+        echo "$__AF_echo_color144_v0__75_9" > /dev/null 2>&1
+        __AF_setup147_v0='';
         return 1
 fi
     local architecture="${__5_architectures[$(echo ${choice} '-' 1 | bc -l | sed '/\./ s/\.\{0,1\}0\{1,\}$//')]}"
-    echo_color__143_v0 "${architecture} selected" "${__3_BLUE}";
-    __AF_echo_color143_v0__56_5="$__AF_echo_color143_v0";
-    echo "$__AF_echo_color143_v0__56_5" > /dev/null 2>&1
+    echo_color__144_v0 "${architecture} selected" "${__3_BLUE}";
+    __AF_echo_color144_v0__81_5="$__AF_echo_color144_v0";
+    echo "$__AF_echo_color144_v0__81_5" > /dev/null 2>&1
     # Downloads the selected binary
-    download_rlsd__144_v0 "rlsd-${architecture}";
-    __AF_download_rlsd144_v0__59_8="$__AF_download_rlsd144_v0";
-    if [ "$__AF_download_rlsd144_v0__59_8" != 0 ]; then
-        echo_color__143_v0 "Install Succeeded" "${__2_GREEN}";
-        __AF_echo_color143_v0__59_46="$__AF_echo_color143_v0";
-        echo "$__AF_echo_color143_v0__59_46" > /dev/null 2>&1
+    download_rlsd__145_v0 "rlsd-${architecture}";
+    __AF_download_rlsd145_v0__84_8="$__AF_download_rlsd145_v0";
+    if [ "$__AF_download_rlsd145_v0__84_8" != 0 ]; then
+        echo_color__144_v0 "Install Succeeded" "${__2_GREEN}";
+        __AF_echo_color144_v0__84_46="$__AF_echo_color144_v0";
+        echo "$__AF_echo_color144_v0__84_46" > /dev/null 2>&1
 else
-        echo_color__143_v0 "Install Failed, check the above error message" "${__1_RED}";
-        __AF_echo_color143_v0__60_11="$__AF_echo_color143_v0";
-        echo "$__AF_echo_color143_v0__60_11" > /dev/null 2>&1
-        __AF_setup145_v0='';
+        echo_color__144_v0 "Install Failed, check the above error message" "${__1_RED}";
+        __AF_echo_color144_v0__85_11="$__AF_echo_color144_v0";
+        echo "$__AF_echo_color144_v0__85_11" > /dev/null 2>&1
+        __AF_setup147_v0='';
         return 1
 fi
-    __AF_setup145_v0=1;
+    # Setup rlsd user
+    input_confirm__96_v0 "Setup rlsd user? (recommended)" 1;
+    __AF_input_confirm96_v0__88_8="$__AF_input_confirm96_v0";
+    if [ "$__AF_input_confirm96_v0__88_8" != 0 ]; then
+        make_user__146_v0 ;
+        __AS=$?;
+if [ $__AS != 0 ]; then
+            __AF_setup147_v0=0;
+            return 0
+fi;
+        __AF_make_user146_v0__88_63="$__AF_make_user146_v0";
+        echo "$__AF_make_user146_v0__88_63" > /dev/null 2>&1
+fi
+    __AF_setup147_v0=1;
     return 0
 }
 declare -r args=("$0" "$@")
     # Checks if the user is root
     is_root__98_v0 ;
-    __AF_is_root98_v0__67_8="$__AF_is_root98_v0";
-    if [ "$__AF_is_root98_v0__67_8" != 0 ]; then
-        echo_color__143_v0 "Root check passed" "${__2_GREEN}";
-        __AF_echo_color143_v0__68_9="$__AF_echo_color143_v0";
-        echo "$__AF_echo_color143_v0__68_9" > /dev/null 2>&1
-        setup__145_v0 ;
+    __AF_is_root98_v0__95_8="$__AF_is_root98_v0";
+    if [ "$__AF_is_root98_v0__95_8" != 0 ]; then
+        echo_color__144_v0 "Root check passed" "${__2_GREEN}";
+        __AF_echo_color144_v0__96_9="$__AF_echo_color144_v0";
+        echo "$__AF_echo_color144_v0__96_9" > /dev/null 2>&1
+        setup__147_v0 ;
         __AS=$?;
-        __AF_setup145_v0__70_28="$__AF_setup145_v0";
-        result="$__AF_setup145_v0__70_28"
+        __AF_setup147_v0__98_28="$__AF_setup147_v0";
+        result="$__AF_setup147_v0__98_28"
         if [ ${result} != 0 ]; then
-            echo_color__143_v0 "Setup Finished" "${__2_GREEN}";
-            __AF_echo_color143_v0__72_20="$__AF_echo_color143_v0";
-            echo "$__AF_echo_color143_v0__72_20" > /dev/null 2>&1
+            echo_color__144_v0 "Setup Finished" "${__2_GREEN}";
+            __AF_echo_color144_v0__100_20="$__AF_echo_color144_v0";
+            echo "$__AF_echo_color144_v0__100_20" > /dev/null 2>&1
 else
-            echo_color__143_v0 "Setup Failed" "${__1_RED}";
-            __AF_echo_color143_v0__73_15="$__AF_echo_color143_v0";
-            echo "$__AF_echo_color143_v0__73_15" > /dev/null 2>&1
+            echo_color__144_v0 "Setup Failed" "${__1_RED}";
+            __AF_echo_color144_v0__101_15="$__AF_echo_color144_v0";
+            echo "$__AF_echo_color144_v0__101_15" > /dev/null 2>&1
 fi
 else
-        echo_color__143_v0 "Root check failed; please run as root" "${__1_RED}";
-        __AF_echo_color143_v0__75_9="$__AF_echo_color143_v0";
-        echo "$__AF_echo_color143_v0__75_9" > /dev/null 2>&1
+        echo_color__144_v0 "Root check failed; please run as root" "${__1_RED}";
+        __AF_echo_color144_v0__103_9="$__AF_echo_color144_v0";
+        echo "$__AF_echo_color144_v0__103_9" > /dev/null 2>&1
 fi
